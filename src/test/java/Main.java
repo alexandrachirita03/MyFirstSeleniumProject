eed3b08f8f8b9ea3cf7588bd0c40d6032540c7a2
alexandrachirita03 <alexandra_rusitoru@yahoo.com>
2022-05-05 21:16:05 +0300

    Lab.1

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -3,10 +3,12 @@ public class Main {
 
     public static void main(String[] args) {
         LoginTest login = new LoginTest();
+        RegisterTest register = new RegisterTest();
         login.loginWithValidCredentialsTest();
         login.loginWithInvalidEmail();
         login.loginWithInvalidPasswordTest();
         login.loginWithoutCredentialsTest();
+        register.validRegisterTest();
     }
 
 }
--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,6 +9,19 @@ public class RegisterTest {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.id("firstname")).sendKeys("Chirita");
+        driver.findElement(By.id("middlename")).sendKeys("Georgiana");
+        driver.findElement(By.id("lastname")).sendKeys("Alexandra");
+        driver.findElement(By.id("email_address")).sendKeys("alexandra.rusitoru95@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("pass123");
+        driver.findElement(By.id("confirmation")).sendKeys("pass123");
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
+        driver.close();
+
+
+
 
     }
 }

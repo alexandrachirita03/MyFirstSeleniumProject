e26d654fc9017eefbd18df397bcc18f4ecd7552e
alexandrachirita03 <alexandra_rusitoru@yahoo.com>
2022-05-12 11:04:33 +0300

    Lab.1

--- src/test/java/RegisterTest.java
+++ src/test/java/RegisterTest.java
@@ -21,7 +21,39 @@ public class RegisterTest {
         driver.close();
 
 
+    }
 
+    public void registerWithInvalidEmail(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.id("firstname")).sendKeys("Chirita");
+        driver.findElement(By.id("middlename")).sendKeys("Georgiana");
+        driver.findElement(By.id("lastname")).sendKeys("Alexandra");
+        driver.findElement(By.id("email_address")).sendKeys("alexandra.rusitoru95gmail.com");
+        driver.findElement(By.id("password")).sendKeys("pass123");
+        driver.findElement(By.id("confirmation")).sendKeys("pass123");
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
+        driver.close();
+    }
 
+    public void registerWithBlankSpacesOnMandatoryFields(){
+        System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://testfasttrackit.info/selenium-test/");
+        driver.findElement(By.cssSelector("#header > div > div.skip-links > div > a > span.label")).click();
+        driver.findElement(By.cssSelector("#header-account > div > ul > li:nth-child(5) > a")).click();
+        driver.findElement(By.id("firstname")).sendKeys(" ");
+        driver.findElement(By.id("middlename")).sendKeys("Georgiana ");
+        driver.findElement(By.id("lastname")).sendKeys(" ");
+        driver.findElement(By.id("email_address")).sendKeys(" ");
+        driver.findElement(By.id("password")).sendKeys(" ");
+        driver.findElement(By.id("confirmation")).sendKeys(" ");
+        driver.findElement(By.cssSelector("#form-validate > div.buttons-set > button > span > span")).click();
+        driver.close();
     }
 }
